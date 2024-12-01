e445455cd93d2ca40788b47482d57d71c1f4ce97
Nelson Huamán H. <libra.nelsonhuaman@gmail.com>
2024-11-30 22:30:22 -0500

    --

--- src/layout/RutaProtegida.jsx
+++ src/layout/RutaProtegida.jsx
@@ -1,25 +1,25 @@
-import { Outlet, Navigate } from "react-router-dom";
-import Header from "../components/header";
-import Footer from "../components/Footer";
+import { Outlet, Navigate } from 'react-router-dom'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import useAuth from '../hooks/useAuth';
 
 
 const RutaProtegida = () => {
 
-   const { auth, cargando } = useAuth();
+   const { auth, cargando } = useAuth()
 
-   if(cargando) return 'Cargando...';
+   if(cargando) return 'cargando...'
    return (
       <>
          <Header />
             {auth?._id ? (
-               <main className="container mx-auto mt-10">
-                  <Outlet />
+               <main className='container mx-auto mt-10'>
+                  <Outlet /> 
                </main>
-            ) : <Navigate to="/" /> }
+            ): <Navigate to="/" /> }
          <Footer />
       </>
    )
-}
+};
 
 export default RutaProtegida;
